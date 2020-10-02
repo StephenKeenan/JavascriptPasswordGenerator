2f00b2ddd11df90042f894f0aef7f8ed2da150e5
Stephen Keenan <stephenkeenan02@gmail.com>
2020-10-02 19:36:57 -0400

    Third commit

--- develop/script.js
+++ develop/script.js
@@ -13,7 +13,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -21,16 +20,22 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
 
+// An array of all characters
   var allCharacters = [];
 
+// Final array returned as the password
   var password = [];
 
+// Password Generator prompt box
   var characterCount = prompt("Choose a password length between 8 and 128 characters");
-  if (isNan(characterCount)) {
+  
+// Checks that user has entered a numerical value
+  if (isNaN(characterCount)) {
 
     return "The value you entered is not accepted";
   }
 
+  // Checks that user's character count selected in within the accepted range
   if (characterCount < 8 || characterCount > 128) {
 
     alert("Please choose a password length between 8 and 128 characters");
@@ -38,41 +43,53 @@ function generatePassword() {
     return "Try Again";
   }
 
+  // If "cancel" is chosen by the user, browser will not include upper case characters
   var upperCase = confirm("Do you want to include upper case characters?");
 
+  // If "ok" is chosen by the user, browser will include upper case characters
   if (upperCase) {
     for (var i = 0; i < upper.length; i++) {
       allCharacters.push(upper[i]);
     }
   }
 
+  // If "cancel" is chosen by the user, browser will not include lower case characters
   var lowerCase = confirm("Do you want to include lower case characters?")
 
+  // If "ok" is chosen by the user, browser will include lower case characters
   if (lowerCase) {
     for (var j = 0; j < lower.length; j++) {
       allCharacters.push(lower[j]);
     }
   }
 
+  // If "cancel" is chosen by the user, browser will not include special characters
   var specialCharacter = confirm("Do you want to include special characters?")
 
+  // If "ok" is chosen by the user, browser will include special characters
   if (specialCharacter) {
     for (var k = 0; k < special.length; k++) {
       allCharacters.push(special[k]);
     }
   }
 
+  // If "cancel" is chosen by the user, browser will not include numbers
   var numbers = confirm("Do you want to include numbers?")
 
+  // If "ok" is chosen by the user, browser will include numbers
   if (numbers) {
     for (var l = 0; l < number.length; l++) {
       allCharacters.push(number[l]);
     }
 
+    // Makes sure return's length is same amount chosen by characterCount function
     for (var f = 0; f < characterCount; f++) {
+
+    // Pushes random selections from the allCharacters array into a password array
       password.push(allCharacters[Math.floor(Math.random() * allCharacters.length)]);
     }
 
+    // Final returned password
     var final = password.join("");
     return final;
   }
